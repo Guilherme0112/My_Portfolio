6d1f6f1de5d4245bc80634cf755006fab42160d4
Guilherme0112 <guimendesmen124@gmail.com>
2025-11-16 09:59:18 -0300

    atualizando curriculo

--- src/components/ProjectBlock/ProjectBlock.jsx
+++ src/components/ProjectBlock/ProjectBlock.jsx
@@ -25,17 +25,6 @@ export default function ProjectBlock() {
             ]
         },
         {
-            name: "Verval",
-            descricao: 'Gerenciador de validade focado em comerciantes que precisam controlar produtos com datas de vencimento próximas. Permite cadastrar detalhadamente os produtos, incluindo informações essenciais para monitoramento eficiente. O sistema envia alertas preventivos, garantindo que o usuário seja avisado antes que os produtos expirem, evitando perdas e facilitando a rotatividade do estoque. A interface combina agilidade e clareza, possibilitando consultas rápidas e gestão precisa do inventário, otimizando a organização e a tomada de decisão no comércio.​​',
-            github: "",
-            deploy: "",
-            stackImages: [
-                "https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png",
-                "https://cdn.worldvectorlogo.com/logos/spring-3.svg",
-                "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
-            ]
-        },
-        {
             name: "ConfeitaAqui",
             descricao: 'Aplicação web robusta voltada para o segmento de confeitarias, oferecendo funcionalidades para cadastro detalhado de estabelecimentos e seus produtos. A interface foi pensada para ser moderna e intuitiva, facilitando a navegação e o uso tanto para administradores quanto para usuários finais. Um diferencial importante é a integração com mapas, que permite ao usuário localizar as confeitarias mais próximas à sua posição atual, tornando a experiência mais prática e direcionada. O sistema atende tanto às necessidades de gestão quanto à usabilidade, garantindo eficiência e satisfação no uso diário.​',
             github: "",
@@ -47,20 +36,6 @@ export default function ProjectBlock() {
                 "https://avatars.githubusercontent.com/u/47703742?s=280&v=4",
                 "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg",
                 "https://cdn-icons-png.flaticon.com/512/5968/5968342.png",
-            ]
-        },
-        {
-            name: "Curriculados",
-            descricao: 'Plataforma prática e acessível para criação rápida de currículos personalizados. O usuário escolhe entre diversos modelos pré-formatados e simplesmente preenche um formulário com suas informações, que são automaticamente inseridas no template selecionado. O sistema simplifica o processo de elaboração do currículo, eliminando a necessidade de conhecimento técnico em design ou formatação, garantindo um resultado profissional com pouco esforço. Ideal para quem busca agilidade e qualidade na apresentação do seu perfil profissional.​​',
-            github: "",
-            deploy: "",
-            stackImages: [
-                "https://cdn-icons-png.flaticon.com/512/732/732212.png",
-                "https://cdn-icons-png.flaticon.com/512/732/732190.png",
-                "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg", // PHP
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg", // Laravel
-                "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" // MySQL
             ]
         },
         {
@@ -93,20 +68,6 @@ export default function ProjectBlock() {
             ]
         },
         {
-            name: "DashFreela",
-            descricao: 'Dashboard pensado para freelancers que precisam gerenciar seus serviços de forma organizada e eficiente. A interface é clara e responsiva, facilitando a visualização e o controle das tarefas em andamento. O sistema permite cadastrar, acompanhar e atualizar projetos, ajudando no planejamento e no cumprimento de prazos. A combinação de tecnologias garante um desempenho estável e uma experiência de uso fluida, atendendo às demandas específicas do trabalho autônomo.​',
-            github: "",
-            deploy: "",
-            stackImages: [
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", // HTML5
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", // CSS3    
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg", // Bootstrap
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", // JavaScript
-                "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg", // PHP
-                "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" // MySQL
-            ]
-        },
-        {
             name: "ControlAccess",
             descricao: 'Sistema para gestão de correspondências que centraliza o cadastro e o monitoramento dos recebimentos. O administrador registra as correspondências e aciona notificações por e-mail para os usuários envolvidos, mantendo-os informados. Os clientes podem acessar a plataforma para visualizar os registros e autorizar a abertura da correspondência, garantindo controle e transparência no processo. Após a autorização, o administrador anexa o conteúdo digitalizado, que gera automaticamente uma nova notificação para o usuário, fechando o ciclo de comunicação de forma eficiente e segura.​',
             github: "",
@@ -133,6 +94,20 @@ export default function ProjectBlock() {
                 "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg",
                 "https://cdn-icons-png.flaticon.com/512/5968/5968342.png",
                 "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png",
+            ]
+        },
+        {
+            name: "Rede",
+            descricao: "Aplicação full stack inspirada no Twitter, construída com Node.js, React com TypeScript, Redux, MongoDB, JWT e Sass. O sistema oferece criação de postagens, comentários, curtidas em comentários, busca dinâmica de usuários e páginas individuais para cada perfil. A API segue boas práticas de organização e segurança com autenticação JWT, enquanto o front-end utiliza Redux para controle previsível de estado e Sass para estilização modular. O resultado é uma rede social funcional, estável e estruturada, demonstrando integração eficiente entre diversas tecnologias atuais.​",
+            github: "https://github.com/Guilherme0112/Rede",
+            deploy: "",
+            video: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7395633760316194817?compact=1",
+            setarLinkDoVideo: () => { setarLinkDoVideo("https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7395633760316194817?compact=1") },
+            stackImages: [
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png",
+                "https://images.archbee.com/PL8X94efBsjvhfQV3wyyj-fMzoU2zBbnzXG5pDzUALe-20250908-073345.png?format=webp",
+                "https://img.icons8.com/color/512/sass.png",
             ]
         }
     ];
